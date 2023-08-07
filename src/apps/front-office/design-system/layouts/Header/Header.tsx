9737af6f2b78dcbb9a65e9e1adcae49590909ab6
hebataha <amltaha866@gmail.com>
2023-08-07 11:59:56 +0300

    header implementaion

--- src/apps/front-office/design-system/layouts/Header/Header.tsx
+++ src/apps/front-office/design-system/layouts/Header/Header.tsx
@@ -1,5 +1,34 @@
+import { Link } from "@mongez/react-router";
 import React from "react";
+import styles from "./style.module.scss";
 
 export default function Header() {
-  return <h3>This Is A Header</h3>;
+  return (
+    <header>
+      <nav>
+        <div className={styles.logo} >
+        logo
+        </div>
+        <div className={styles.navBar} >
+        logo
+    
+        <li>
+          <Link>Home</Link>
+        </li>
+        <li>
+          <Link>About</Link>
+        </li>
+        <li>
+          <Link>Reservation</Link>
+        </li>
+        <li>
+          <Link>Home</Link>
+        </li>
+        <li>
+          <Link>Home</Link>
+        </li>
+        </div>
+      </nav>
+    </header>
+  );
 }
