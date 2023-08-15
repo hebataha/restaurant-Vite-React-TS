a5d30d0d626fba0515f76438f263d7d0c9584cd9
hebataha <amltaha866@gmail.com>
2023-08-15 20:55:22 +0300

    main dishes

--- src/apps/front-office/home/MainDishes/MainDishes.tsx
+++ src/apps/front-office/home/MainDishes/MainDishes.tsx
@@ -10,24 +10,56 @@ export default function MainDishes(props: MainDishesProps) {
       <div className={styles.mainDishes}>
         <div className="container">
           <div className={styles.mainDishesContainer}>
-            <img src={TitleShape} />
-            <h1>Starters & Main Dishes</h1>
-            <p>
-              It’s the story of an everlasting love affair, Dieter Delicioz and
-              the Atlantic Ocean in the big air.
-            </p>
-            <div className={styles.menuWrapper}>
-              <div className={styles.menuImg}>
-                <img src={Burger} />
+            <div className={styles.menuTitle}>
+              <img src={TitleShape} />
+              <h1>Starters & Main Dishes</h1>
+              <p>
+                It’s the story of an everlasting love affair, Dieter Delicioz
+                and the Atlantic Ocean in the big air.
+              </p>
+            </div>
+            <div className={styles.menuList}>
+              <div className={styles.menuWrapper}>
+                <div className={styles.menuImg}>
+                  <img src={Burger} />
+                </div>
+                <div className={styles.menuInfo}>
+                  <h3>Chicha Morada</h3>
+                  <p>Beetroot And Datterini, Goat Cheese, Ricotta</p>
+                </div>
+                <div className={styles.menuButton}>
+                  <button>$33.00</button>
+                </div>
               </div>
-              <div className={styles.menuInfo}>
-                <h3>Chicha Morada</h3>
-                <p>Beetroot And Datterini, Goat Cheese, Ricotta</p>
+
+              <div className={styles.menuWrapper}>
+                <div className={styles.menuImg}>
+                  <img src={Burger} />
+                </div>
+                <div className={styles.menuInfo}>
+                  <h3>Chicha Morada</h3>
+                  <p>Beetroot And Datterini, Goat Cheese, Ricotta</p>
+                </div>
+                <div className={styles.menuButton}>
+                  <button>$33.00</button>
+                </div>
+              </div>
+
+              
+              <div className={styles.menuWrapper}>
+                <div className={styles.menuImg}>
+                  <img src={Burger} />
+                </div>
+                <div className={styles.menuInfo}>
+                  <h3>Chicha Morada</h3>
+                  <p>Beetroot And Datterini, Goat Cheese, Ricotta</p>
+                </div>
+                <div className={styles.menuButton}>
+                  <button>$33.00</button>
+                </div>
               </div>
             </div>
-            <div className={styles.menuButton}>
-              <button>$33.00</button>
-            </div>
+       
           </div>
         </div>
       </div>
